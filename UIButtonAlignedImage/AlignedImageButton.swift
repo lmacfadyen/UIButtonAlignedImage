308bfd8fd0fea6406198234357d91c8496a40be3
Lawrence F MacFadyen <larry.macfadyen@gmail.com>
2017-08-14 22:18:49 -0400

    Comments updated

--- UIButtonAlignedImage/AlignedImageButton.swift
+++ UIButtonAlignedImage/AlignedImageButton.swift
@@ -21,7 +21,7 @@ class AlignedImageButton: UIButton {
         super.init(coder: aDecoder)
         // If button in storyboard is Custom, you'll need to set
         // title color for control states and optionally the font
-        // I've set mine to System, so uncomment next two lines if Custom
+        // I've set mine to System, so uncomment next three lines if Custom
 
         //self.setTitleColor(tintColor, for: .normal)
         //self.setTitleColor(tintColor.withAlphaComponent(0.3), for: .highlighted)
